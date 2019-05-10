0d2ba7de6eda17eda4c29e3dcab0705278812c13
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-10 15:31:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 if (name === "Susan") {
   console.log('Yep that\'s right')
+} else {
+  console.log('Well that\'s not right...')
 }
 
 // Don't worry about this
